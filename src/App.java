735d74c1e2b9c0417019135785fc7eb25d214439
Dhanush2004-pixwik <dhanushkarthik.r@pixwik.com>
2026-06-29 15:04:44 +0530

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,19 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("   WELCOME TO JENKINS PROJECT");
+        System.out.println("=================================");
+
+        System.out.println("Application : Hello Jenkins");
+
+        System.out.println("Version     : 1.0");
+
+        System.out.println("Status      : SUCCESS");
+
+        System.out.println("Built using Jenkins");
+
+    }
+
+}
---
+++ src/README.md
@@ -0,0 +1,9 @@
+# Hello Jenkins Project
+
+This project is created to learn Jenkins CI.
+
+Technologies Used
+
+- Java
+- Git
+- Jenkins
