1b3bab5cdf4c65b39edf9ff0086c57a8ea36eeb5
Dhanush2004-pixwik <dhanushkarthik.r@pixwik.com>
2026-06-30 10:01:12 +0530

    Rebuild

--- src/App.java
+++ src/App.java
@@ -18,10 +18,9 @@ public class App {
 
         System.out.println("WebHook Successfully Tested");
 
-        System.out.println("Git Automation Successful")
+        System.out.println("Git Automation Successful");
 
         
-
     }
 
 }
